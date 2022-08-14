d09b1ddc231526645f138ba098cf70368dfdd9fd
Mayar Adel <mariomamdouh@aucegypt.edu>
2022-08-13 20:15:09 -0700

    cart

---
+++ Cart/CartScreenViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CartViewViewController.swift
+//  project draft 1
+//
+//  Created by Mayar Adel on 8/13/22.
+//
+
+import UIKit
+
+class CartScreenViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let Cell = Table.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return Cell
+    }
+    
+    
+
+    @IBOutlet weak var Table: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
