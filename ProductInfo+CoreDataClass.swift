e966a00f72aa94d4be84aa848a2c6cfc126e7616
Mayar Adel <71149364+haaleem@users.noreply.github.com>
2022-08-14 11:21:19 -0700

    dfs

---
+++ ProductInfo+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ProductInfo+CoreDataClass.swift
+//  project draft 1
+//
+//  Created by Mayar Adel on 8/14/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ProductInfo)
+public class ProductInfo: NSManagedObject {
+
+}
